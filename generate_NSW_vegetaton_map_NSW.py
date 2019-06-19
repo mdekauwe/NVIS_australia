3408eb0eb373c0a23746d5ff0506c11038b6a5db
mdekauwe <mdekauwe@gmail.com>
2019-06-19 12:48:57 +1000

    Added subset code

--- generate_NSW_vegetaton_map_NSW.py
+++ generate_NSW_vegetaton_map_NSW.py
@@ -17,12 +17,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import xarray as xr
 
+
 def main(fname):
 
     ds = xr.open_dataset(fname)
 
-    print(ds)
 
+    # subset by NSW-ish, extending through victoria too.
+    # cdo sellonlatbox,138., 154.,-40.0, -28. in.nc out.nc
+    lat_bounds = [-40.0, -28.]
+    lon_bounds = [138., 154.]
+    bounds = lat_bounds + lon_bounds
+
+    """
+    lats = ds['latitude'][:]
+    lons = ds['longitude'][:]
+    latli = np.argmin(np.abs(lats - lat_bounds[1])).values
+    latui = np.argmin(np.abs(lats - lat_bounds[0])).values
+    lonli = np.argmin(np.abs(lons - lon_bounds[0])).values
+    lonui = np.argmin(np.abs(lons - lon_bounds[1])).values
+    """
+
+    #print([latli.values,latui,lonli,lonui])
+    #[latli:latui,lonli:lonui]
+
+    #print(np.unique(biomes))
+    #print(ds.biome_code[latli:latui,lonli:lonui].shape)
+    #plt.imshow(ds.biome_code)
+    #plt.colorbar()
+    #plt.show()
 
 if __name__ == "__main__":
 
