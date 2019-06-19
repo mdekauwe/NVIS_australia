076f7a5e311974b91e037cc7a7f7b010e34f8def
mdekauwe <mdekauwe@gmail.com>
2019-06-19 13:02:41 +1000

    Added classes

---
+++ test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""
+Generate six vegetation types land cover map for NSW
+
+That's all folks.
+"""
+__author__ = "Martin De Kauwe"
+__version__ = "1.0 (19.06.2019)"
+__email__ = "mdekauwe@gmail.com"
+
+import os
+import sys
+import glob
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+
+
+def main(fname):
+
+    
+
+if __name__ == "__main__":
+
+    fname = "data/w001001.adf"
+    main(fname)
