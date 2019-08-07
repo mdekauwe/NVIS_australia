bf40d64643f68a75e39f41a2cbecf30f75735fe1
mdekauwe <mdekauwe@gmail.com>
2019-08-07 12:36:11 +1000

    Added regrid script

---
+++ remap_NVIS_to_GSWP3_grid.sh
@@ -0,0 +1,8 @@
+#cdo remapnn,/g/data1/wd9/MetForcing/Global/GSWP3_2017/Tair/GSWP3.BC.Tair.3hrMap.1929.nc  \
+#            nc_files/patch_frac/patch_frac.nc \
+#            nc_files/patch_frac/patch_frac_0.5.nc
+
+
+cdo remapnn,/Users/mdekauwe/Desktop/GSWP3_2017/Tair/GSWP3.BC.Tair.3hrMap.1929.nc  \
+            data/SE_aus_veg_types.nc \
+            data/SE_aus_veg_types.nc_0.5.nc
