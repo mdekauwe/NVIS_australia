0f2200ceae0403ce3eea85b3a5caabcf674e288b
mdekauwe <mdekauwe@gmail.com>
2019-08-26 14:24:45 +1000

    Added awap grid translation

--- remap_NVIS_to_GSWP3_grid.sh
+++ remap_NVIS_to_GSWP3_grid.sh
@@ -1,8 +1,14 @@
-#cdo remapnn,/g/data1/wd9/MetForcing/Global/GSWP3_2017/Tair/GSWP3.BC.Tair.3hrMap.1929.nc  \
-#            nc_files/patch_frac/patch_frac.nc \
-#            nc_files/patch_frac/patch_frac_0.5.nc
+#!/bin/bash
+
+#cdo remapnn,/Users/mdekauwe/Desktop/GSWP3.BC.Tair.3hrMap.1929.nc  \
+#            data/SE_aus_veg_types.nc \
+#            data/E_aus_veg_types_AWAP_grid.nc
+
+#cdo remapcon,/Users/mdekauwe/Desktop/GSWP3.BC.Tair.3hrMap.1929.nc  \
+#            data/SE_aus_veg_types.nc \
+#            data/E_aus_veg_types_AWAP_grid.nc
 
 
-cdo remapnn,/Users/mdekauwe/Desktop/GSWP3.BC.Tair.3hrMap.1929.nc  \
+cdo remapnn,/Users/mdekauwe/Desktop/awap_grid_file_for_martin.nc  \
             data/SE_aus_veg_types.nc \
-            data/SE_aus_veg_types.nc_0.5.nc
+            data/SE_aus_veg_types_AWAP_grid.nc
